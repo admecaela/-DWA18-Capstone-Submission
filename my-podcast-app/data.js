3062905f48b027e545ff5e89e2e112cd9a4f4f50
Mecaela <127962504+admecaela@users.noreply.github.com>
2023-11-25 02:19:13 +0200

    Add files via upload

--- my-podcast-app/data.js
+++ my-podcast-app/data.js
@@ -1,4 +1,18 @@
-export default {
+async function fetchPodcastData() {
+  try {
+    const response = await fetch('https://podcast-api.netlify.app/shows');
+    if (!response.ok) {
+      throw new Error('HTTP error, status = ' + response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error('Error fetching podcast data:', error);
+    return null;
+  }
+}
+
+  export default {
     "success": true,
     "data": {
         "id": "10716",
