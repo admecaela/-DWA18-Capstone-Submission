d568810623c05aea11568711bce88bf137e16202
Mecaela <127962504+admecaela@users.noreply.github.com>
2023-11-25 02:20:49 +0200

    Add files via upload

--- my-podcast-app/src/app.js
+++ my-podcast-app/src/app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchPodcastData from '../data';
 import '../src/styles.css'
 import Navbar from '../src/components/Navbar.js'
 import Filter from '../src/components/Filter.js'
@@ -10,9 +11,38 @@ import GenreSorted from './components/GenreSorted.js';
 import Home from './components/Home.js'
 
 function App() {
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const data = await fetchPodcastData();
+      setPodcasts(data || []);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching podcast data:', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Welcome to MecaFunk Podcast App</h1>
+      {loading ? <div>Loading...</div> : null}
+      {podcasts.length > 0 ? (
+        <div>
+          <h2>{podcasts[0].title}</h2>
+          <p>{podcasts[0].summary}</p>
+        </div>
+      ) : (
+        <div>No podcasts fetched.</div>
+      )}
+      <button onClick={fetchData}>Load Podcast</button>
+      
       <Navbar />
       <Filter />
       <Socials />
@@ -21,6 +51,7 @@ function App() {
       <SearchPanel />
       <GenreSorted />
       <Home />
+      <fetchPodcastData />
     </div>
   );
 }
